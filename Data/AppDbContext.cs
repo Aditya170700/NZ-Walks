c623d874ab68118baaed5f6772e50c49c1851c93
macbookair <52729836+Aditya170700@users.noreply.github.com>
2023-03-03 14:59:18 +0700

    Setup db (entity framework,migrations, etc).

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using NZ_Walks.Models.Domain;
+
+namespace NZ_Walks.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Region> Regions { get; set; }
+		public DbSet<WalkDifficulty> WalkDifficultys { get; set; }
+		public DbSet<Walk> Walks { get; set; }
+    }
+}
+
---
+++ Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace NZ_Walks.Models.Domain
+{
+	public class WalkDifficulty
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+	}
+}
+
